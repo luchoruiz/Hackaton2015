2931e13c1af3b7952f78fb81f3ecc19bc3a8a42e
Luciano Ruiz <lucianof.ruiz@gmail.com>
2015-09-26 01:27:29 -0300

    mergin changes

--- app/src/main/java/com/android/desafioaudionews/activities/MainActivity.java
+++ app/src/main/java/com/android/desafioaudionews/activities/MainActivity.java
@@ -16,11 +16,11 @@ import android.view.MenuItem;
 import com.android.desafioaudionews.R;
 import com.android.desafioaudionews.adapters.CustomTabPagerAdapter;
 import com.android.desafioaudionews.database.DatabaseHelper;
-<<<<<<< HEAD
+
 import com.android.desafioaudionews.models.Note;
-=======
+
 import com.android.desafioaudionews.models.Category;
->>>>>>> bc38ffe7e75f436978743393c5975fb3b1d9389c
+
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
 import java.util.List;
@@ -39,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
     NavigationView view;
     @InjectView(R.id.tabLayout)
     TabLayout tabLayout;
-    private DatabaseHelper databaseHelper;
-
     private DatabaseHelper databaseHelper;
 
     @Override
